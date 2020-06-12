a72f1c35e07174057085cc804f0040102d46f43f
terence tsao <terence@prysmaticlabs.com>
2020-06-11 19:13:22 -0700

    Light client helpers

---
+++ beacon-chain/core/helpers/light.go
@@ -0,0 +1,75 @@
+package helpers
+
+import (
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/beacon-chain/state"
+	"github.com/prysmaticlabs/prysm/shared/params"
+)
+
+// PackCompactValidator packs validator index, slashed status and compressed balance into a single uint value.
+//
+// Spec pseudocode definition:
+// Spec code (https://github.com/ethereum/eth2.0-specs/blob/7a770186b5ba576bf14ce496dc2b0381d169840e/specs/phase1/beacon-chain.md):
+// def pack_compact_validator(index: ValidatorIndex, slashed: bool, balance_in_increments: uint64) -> uint64:
+//    """
+//    Create a compact validator object representing index, slashed status, and compressed balance.
+//    Takes as input balance-in-increments (// EFFECTIVE_BALANCE_INCREMENT) to preserve symmetry with
+//    the unpacking function.
+//    """
+//    return (index << 16) + (slashed << 15) + balance_in_increments
+func PackCompactValidator(index uint64, slashed bool, balanceIncrements uint64) uint64 {
+	if slashed {
+		return (index << 16) + (1 << 15) + balanceIncrements
+	}
+	return (index << 16) + (0 << 15) + balanceIncrements
+}
+
+// UnpackCompactValidator unpacks the compact validator object into index, slashed and balance forms.
+//
+// Spec code (https://github.com/ethereum/eth2.0-specs/blob/7a770186b5ba576bf14ce496dc2b0381d169840e/specs/phase1/beacon-chain.md):
+//   def unpack_compact_validator(compact_validator: CompactValidator) -> Tuple[ValidatorIndex, bool, uint64]:
+//    """
+//    Return the index, slashed, effective_balance // EFFECTIVE_BALANCE_INCREMENT of ``compact_validator``.
+//    """
+//    return (
+//        ValidatorIndex(compact_validator >> 16),
+//        (compact_validator >> 15) % 2,
+//        uint64(compact_validator & (2**15 - 1)),
+//    )
+func UnpackCompactValidator(compactValidator uint64) (uint64, bool, uint64) {
+	index := compactValidator >> 16
+	slashed := (compactValidator >> 15) == 1
+	balance := uint64(compactValidator & (1<<15 - 1))
+
+	return index, slashed, balance
+}
+
+// CommitteeToCompactCommittee converts a committee object to compact committee object.
+//
+// Spec code (https://github.com/ethereum/eth2.0-specs/blob/7a770186b5ba576bf14ce496dc2b0381d169840e/specs/phase1/beacon-chain.md):
+//   def committee_to_compact_committee(state: BeaconState, committee: Sequence[ValidatorIndex]) -> CompactCommittee:
+//    """
+//    Given a state and a list of validator indices, outputs the CompactCommittee representing them.
+//    """
+//    validators = [state.validators[i] for i in committee]
+//    compact_validators = [
+//        pack_compact_validator(i, v.slashed, v.effective_balance // EFFECTIVE_BALANCE_INCREMENT)
+//        for i, v in zip(committee, validators)
+//    ]
+//    pubkeys = [v.pubkey for v in validators]
+//    return CompactCommittee(pubkeys=pubkeys, compact_validators=compact_validators)
+func CommitteeToCompactCommittee(state *state.BeaconState, committee []uint64) (*ethpb.CompactCommittee, error) {
+	compactValidators := make([]uint64, len(committee))
+	pubKeys := make([][]byte, len(committee))
+
+	for i := 0; i < len(committee); i++ {
+		v, err := state.ValidatorAtIndex(committee[i])
+		if err != nil {
+			return nil, err
+		}
+		compactValidators[i] = PackCompactValidator(committee[i], v.Slashed, v.EffectiveBalance/params.BeaconConfig().EffectiveBalanceIncrement)
+		pubKeys[i] = v.PublicKey
+	}
+
+	return &ethpb.CompactCommittee{CompactValidators: compactValidators, PubKeys: pubKeys}, nil
+}
